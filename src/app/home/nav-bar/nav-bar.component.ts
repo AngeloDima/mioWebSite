efc168227dd714deb4a33bded43a74b87376a3bf
AngeloDima <Angelodimauro91@gmail.com>
2023-10-01 17:17:16 +0200

    rimpiazzo le routing con una scrollata

--- src/app/home/nav-bar/nav-bar.component.ts
+++ src/app/home/nav-bar/nav-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,6 +7,61 @@ import { Router } from '@angular/router';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent {
+
+  abo() {
+    // Calcola la percentuale dello scorrimento
+    const windowHeight = window.innerHeight;
+    const scrollPercentage = 1.5; // Puoi impostare la percentuale desiderata
+
+    // Sposta la pagina in base alla percentuale
+    window.scrollTo({
+      top: windowHeight * scrollPercentage,
+      behavior: 'smooth' // Questo rende lo scorrimento animato
+    });
+  }
+
+
+  cor() {
+    // Calcola la percentuale dello scorrimento
+    const windowHeight = window.innerHeight;
+    const scrollPercentage = 0.8; // Puoi impostare la percentuale desiderata
+
+    // Sposta la pagina in base alla percentuale
+    window.scrollTo({
+      top: windowHeight * scrollPercentage,
+      behavior: 'smooth' // Questo rende lo scorrimento animato
+    });
+  }
+
+
+
+
+  freel() {
+    // Calcola la percentuale dello scorrimento
+    const windowHeight = window.innerHeight;
+    const scrollPercentage = 6.1; // Puoi impostare la percentuale desiderata
+
+    // Sposta la pagina in base alla percentuale
+    window.scrollTo({
+      top: windowHeight * scrollPercentage,
+      behavior: 'smooth' // Questo rende lo scorrimento animato
+    });
+  }
+
+
+
+  contact() {
+    // Calcola la percentuale dello scorrimento
+    const windowHeight = window.innerHeight;
+    const scrollPercentage = 7.4; // Puoi impostare la percentuale desiderata
+
+    // Sposta la pagina in base alla percentuale
+    window.scrollTo({
+      top: windowHeight * scrollPercentage,
+      behavior: 'smooth' // Questo rende lo scorrimento animato
+    });
+  }
+
 
 }
 
